3e572793d62da64c819241053e73d0c88ddba955
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-10-22 23:34:54 +0900

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    extends: ["airbnb"],
+  },
+];
